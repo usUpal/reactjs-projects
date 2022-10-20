9f43831d17ed2e6d4b1f3c3961f5a5fd98546a3d
usupal <udyan.upal@gmail.com>
2022-10-21 02:35:58 +0600

    08-lorem-ipsum done

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -2,9 +2,41 @@ import React, { useState } from 'react';
 import data from './data';
 function App() {
   
-  
+  const [count, setCount]= useState(0);
+  const [text, setText] = useState([]);  
+
+  const handleClick = (e)=>{
+    e.preventDefault();
+    let amount = parseInt(count)
+    if(amount <= 0){
+      amount = 1
+    }
+    if(amount > 8){
+      amount = 8
+    }
+    setText(data.slice(0,amount))
+  }
   return (
-  <h2>lorem ipsum project setup</h2>
+    <section className='section-center'>
+      <h3>Lorem Ipsum Generator</h3>
+      <form className='lorem form' onSubmit={handleClick}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input type='number'
+                name='amount'
+                id='amount'
+                value={count}
+                onChange={(e)=> setCount(e.target.value)}/>
+        <button type='submit' className='btn'>Generate</button>
+      </form>
+      <article>
+      {text.map((item, index)=>{
+        return(
+          <p key={index}>{item}</p>
+        );
+      })}
+      </article>
+
+    </section>
     )
 }
 
